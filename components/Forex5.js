52416446df50f1aac85c86d273653421101f6d6c
ermankaval <ermankaval@yahoo.com>
2024-03-18 10:42:11 +0300

    favoriler işaretleme hatası düzeltildi

--- components/Forex5.js
+++ components/Forex5.js
@@ -3,22 +3,17 @@ import Pagination from './Pagination';
 import { getFirestore, doc, setDoc, addDoc, getDocs, collection, deleteDoc, where, query } from 'firebase/firestore';
 import { db, auth } from '../components/firebase';
 
-
 const Forex5 = () => {
     const [forexData, setForexData] = useState([]);
-    const [selectedRows, setSelectedRows] = useState([]);
+    const [selectedRows, setSelectedRows] = useState({});
     const [currentPageTop, setCurrentPageTop] = useState(1);
     const [perPage, setPerPage] = useState(10);
-    const [favorites, setFavorites] = useState([]);
 
     const fetchData = async () => {
         try {
+            // Forex verilerini al
             const url = 'https://finans.truncgil.com/v4/today.json';
-            const options = {
-                method: 'GET',
-                headers: {},
-            };
-
+            const options = { method: 'GET', headers: {} };
             const response = await fetch(url, options);
             const result = await response.json();
 
@@ -64,29 +59,18 @@ const Forex5 = () => {
             const data = await fetchData();
 
             const user = auth.currentUser;
-            console.log(user.displayName);
             if (user) {
                 // Firestore'dan favori verileri al
                 const favoritesData = await loadFavoritesFromFirestore();
-                console.log(favoritesData);
-                console.log("bura", forexData.currency);
-                // Favori verilerin tablodaki indekslerini bul
-                const selectedRows = favoritesData.map((favorite) => {
-                    const indexInCurrentPage = data.findIndex((currency) => {
-                        return currency.currency === favorite.symbol;
-                    });
 
-                    if (indexInCurrentPage !== -1) {
-                        return indexInCurrentPage + Math.floor(indexInCurrentPage / perPage);
-                    } else {
-                        return -1; // Geçersiz indeks
-                    }
+                // Favori sembolleri bir nesne olarak sakla
+                const favoritesObj = {};
+                favoritesData.forEach((favorite) => {
+                    favoritesObj[favorite.symbol] = true;
                 });
 
-                console.log("SelectedRow:", selectedRows);
-
-                // Yıldız işaretlemesini sağlamak için selectedRows dizisini güncelleyin
-                setSelectedRows(selectedRows);
+                // Favori sembolleri state'e ayarla
+                setSelectedRows(favoritesObj);
             } else {
                 console.error('User not logged in');
             }
@@ -98,17 +82,12 @@ const Forex5 = () => {
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(async (user) => {
             if (user) {
-                await initializeData(); // await kullanarak initializeData fonksiyonunu bekleyin
-
-                // Forex verilerini konsola yazdırın
-                // console.log("Forex Data:", forexData);
+                await initializeData();
             }
         });
 
         return unsubscribe;
     }, []);
-
-
 
     const loadFavoritesFromFirestore = async () => {
         const data = [];
@@ -116,11 +95,11 @@ const Forex5 = () => {
             const user = auth.currentUser;
             if (user) {
                 const userId = user.uid;
-                const db = getFirestore(); // Firestore nesnesini alın
-                const favoritesCollectionRef = collection(db, 'forex'); // 'forex' koleksiyonunu seçiyoruz
-                const q = query(favoritesCollectionRef, where('userId', '==', userId)); // Kullanıcının favori dövizlerini almak için bir sorgu oluşturuyoruz
+                const db = getFirestore();
+                const favoritesCollectionRef = collection(db, 'forex');
+                const q = query(favoritesCollectionRef, where('userId', '==', userId));
 
-                const querySnapshot = await getDocs(q); // Firestore'dan verileri alıyoruz
+                const querySnapshot = await getDocs(q);
                 querySnapshot.forEach((doc) => {
                     data.push({ id: doc.id, ...doc.data() });
                 });
@@ -135,52 +114,55 @@ const Forex5 = () => {
         }
     };
 
+    const handleRowClick = async (currency) => {
+        try {
+            const updatedSelectedRows = { ...selectedRows };
 
+            // Toggle favori durumunu güncelle
+            if (selectedRows[currency]) {
+                delete updatedSelectedRows[currency];
+                await removeFavoriteFromFirestore(currency);
+            } else {
+                updatedSelectedRows[currency] = true;
+                await saveFavoriteToFirestore(currency);
+            }
 
-
-
-
-
-
-
-    const handleRowClick = async (tableRowIndex) => {
-        // Tablodaki satırın indeksini elde edin
-        const index = currentPageTop * perPage - perPage + tableRowIndex;
-
-        const updatedSelectedRows = [...selectedRows];
-        const selectedIndex = updatedSelectedRows.indexOf(index);
-
-        if (selectedIndex === -1) {
-            // If not already selected, add to the array
-            updatedSelectedRows.push(index);
-
-            // Save selected rows to localStorage
-            localStorage.setItem('selectedRows', JSON.stringify(updatedSelectedRows));
-
-            // Save favorites to Firestore
-            await saveFavoritesToFirestore(index);
-        } else {
-            // If already selected, remove from the array
-            updatedSelectedRows.splice(selectedIndex, 1);
-
-            // Remove from Firestore
-            await removeFavoriteFromFirestore(index);
-
-            // Remove selected rows from localStorage
-            localStorage.setItem('selectedRows', JSON.stringify(updatedSelectedRows));
+            // State'i güncelle
+            setSelectedRows(updatedSelectedRows);
+        } catch (error) {
+            console.error('Error handling row click:', error);
         }
-
-        // Update state with the updatedSelectedRows array
-        setSelectedRows(updatedSelectedRows);
     };
 
+    const removeFavoriteFromFirestore = async (symbol) => {
+        try {
+            const db = getFirestore();
+            const favoritesCollectionRef = collection(db, 'forex');
+            const q = query(favoritesCollectionRef, where('userId', '==', auth.currentUser.uid), where('symbol', '==', symbol));
+            const querySnapshot = await getDocs(q);
 
+            querySnapshot.forEach(async (doc) => {
+                await deleteDoc(doc.ref);
+                console.log('Document successfully deleted!');
+            });
+        } catch (error) {
+            console.error('Error removing data from Firestore:', error);
+        }
+    };
 
-
+    const saveFavoriteToFirestore = async (symbol) => {
+        try {
+            const db = getFirestore();
+            const favoritesCollectionRef = collection(db, 'forex');
+            await addDoc(favoritesCollectionRef, { userId: auth.currentUser.uid, symbol });
+            console.log('Document successfully written!');
+        } catch (error) {
+            console.error('Error saving data to Firestore:', error);
+        }
+    };
 
     const getTriangleColor = (change) => {
         const changeValue = parseFloat(change);
-
         if (changeValue > 0) {
             return 'text-green-500'; // Yeşil üçgen yukarı bakar
         } else if (changeValue < 0) {
@@ -193,61 +175,6 @@ const Forex5 = () => {
     const handlePerPageChange = (e) => {
         setPerPage(Number(e.target.value));
         setCurrentPageTop(1);
-    };
-
-
-    const removeFavoriteFromFirestore = async (index) => {
-        try {
-            const db = getFirestore();
-            const collectionRef = collection(db, 'forex');
-            const selectedCurrency = forexData[index];
-
-            if (selectedCurrency) {
-                const querySnapshot = await getDocs(collectionRef);
-
-                querySnapshot.forEach(async (doc) => {
-                    const data = doc.data();
-                    if (data.userId === auth.currentUser.uid && data.symbol === selectedCurrency.currency) {
-                        await deleteDoc(doc.ref);
-                        console.log('Document successfully deleted!');
-                    }
-                });
-            }
-        } catch (error) {
-            console.error('Error removing data from Firestore:', error);
-        }
-    };
-
-    const saveFavoritesToFirestore = async (index) => {
-        try {
-            const db = getFirestore();
-            const collectionRef = collection(db, 'forex');
-
-            const selectedCurrency = forexData[index];
-
-            if (selectedCurrency) {
-                const symbolToCheck = selectedCurrency.currency;
-                const userId = auth.currentUser.uid;
-
-                // Check if the document with the same symbol and user already exists
-                const querySnapshot = await getDocs(query(collectionRef, where('symbol', '==', symbolToCheck), where('userId', '==', userId)));
-
-                if (querySnapshot.empty) {
-                    const dataToSave = {
-                        userId: userId,
-                        symbol: symbolToCheck,
-                        // Add other fields if needed
-                    };
-
-                    const docRef = await addDoc(collectionRef, dataToSave);
-                    console.log('Document written with ID:', docRef.id);
-                } else {
-                    console.log('Document with the same symbol and user already exists.');
-                }
-            }
-        } catch (error) {
-            console.error('Error saving data to Firestore:', error);
-        }
     };
 
     const indexOfLastRow = currentPageTop * perPage;
@@ -280,22 +207,22 @@ const Forex5 = () => {
                         <th className="py-2 px-4 border-b text-left">Currency</th>
                         <th className="py-2 px-4 border-b text-left">Rate</th>
                         <th className="py-2 px-4 border-b text-left">Change (%)</th>
-                        <th className="py-2 px-4 border-b text-left"></th>
+                        <th className="py-2 px-4 border-b text-left">Direction</th>
                     </tr>
                 </thead>
                 <tbody>
                     {currentRows.map((currency, index) => (
                         <tr
                             key={currency.currency}
-                            className={`group hover:bg-gray-300 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'} ${selectedRows.includes(indexOfFirstRow + index) ? 'selected-row' : ''}`}
-                            onClick={() => handleRowClick(indexOfFirstRow + index)}
+                            className={`group hover:bg-gray-300 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}`}
+                            onClick={() => handleRowClick(currency.currency)}
                         >
                             <td className="py-2 px-4 border-b text-left">
-                                {selectedRows.includes(indexOfFirstRow + index) ? <span className="full-star"></span> : <span className="empty-star"></span>}
+                                <span className={selectedRows[currency.currency] ? 'full-star' : 'empty-star'}></span>
                             </td>
                             <td className="py-2 px-4 border-b text-left text-sm">{currency.currency}</td>
                             <td className="py-2 px-4 border-b text-left text-sm">{currency.rate}</td>
-                            <td className={`py-2 px-4 border-b text-left font-bold text-sm ${parseFloat(currency.change) === 0 ? 'text-blue-500' : getTriangleColor(currency.change)}`}>
+                            <td className={`py-2 px-4 border-b text-left font-bold text-sm  ${parseFloat(currency.change) === 0 ? 'text-blue-500' : getTriangleColor(currency.change)}`}>
                                 {`% ${currency.change}`}
                             </td>
                             <td className={`py-2 px-4 border-b text-left ${getTriangleColor(currency.change)}`}>{parseFloat(currency.change) < 0 ? '▼' : parseFloat(currency.change) > 0 ? '▲' : '-'}</td>
@@ -304,6 +231,7 @@ const Forex5 = () => {
                 </tbody>
             </table>
 
+
             <Pagination
                 perPage={perPage}
                 totalRows={forexData.length}
@@ -311,8 +239,7 @@ const Forex5 = () => {
                 paginate={setCurrentPageTop}
             />
 
-            {/* Selected Rows Table */}
-            <h2 className="mt-4 mb-2 text-lg font-semibold">Takip Listem</h2>
+
             <table className="min-w-full bg-white border border-gray-300 rounded-lg overflow-hidden">
                 <thead>
                     <tr>
@@ -324,18 +251,18 @@ const Forex5 = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {selectedRows.map((selectedIndex, selectedRowIndex) => {
-                        const selectedCurrency = forexData[selectedIndex];
+                    {Object.entries(selectedRows).map(([currency, isSelected], index) => {
+                        const selectedCurrency = forexData.find((data) => data.currency === currency);
 
                         if (selectedCurrency) {
                             return (
                                 <tr
                                     key={selectedCurrency.currency}
-                                    className={`hover:bg-gray-300 ${selectedRowIndex % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}`}
-                                    onClick={() => handleRowClick(selectedIndex)}
+                                    className={`hover:bg-gray-300 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}`}
+                                    onClick={() => handleRowClick(selectedCurrency.currency)}
                                 >
                                     <td className="py-2 px-4 border-b text-left">
-                                        <span className={selectedRows.includes(selectedIndex) ? "full-star" : "empty-star"}></span>
+                                        <span className={isSelected ? "full-star" : "empty-star"}></span>
                                     </td>
                                     <td className="py-2 px-4 border-b text-left text-sm">{selectedCurrency.currency}</td>
                                     <td className="py-2 px-4 border-b text-left text-sm">{selectedCurrency.rate}</td>
@@ -352,6 +279,8 @@ const Forex5 = () => {
                 </tbody>
             </table>
         </div>
+
+
     );
 };
 
